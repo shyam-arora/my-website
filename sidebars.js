41ab800250e99254de5acac53084905488547d4d
Shyam Arora <shyarora@cisco.com>
2020-10-26 20:52:36 +0530

    Category added

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,44 @@
 module.exports = {
-  someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
-  },
+  docs: [
+    {
+      type: "category",
+      label: "Getting started",
+      items: ["doc1"],
+    },
+    {
+      type: "category",
+      label: "HTML/CSS",
+      items: ["html/mdx"],
+    },
+    {
+      type: "category",
+      label: "Javascript",
+      items: ["javascript/mdx"],
+    },
+    {
+      type: "category",
+      label: "Javascript Polyfills",
+      items: ["javascript-polyfills/mdx"],
+    },
+    {
+      type: "category",
+      label: "Leetcode",
+      items: ["leetcode/mdx"],
+    },
+    {
+      type: "category",
+      label: "React JS",
+      items: ["reactjs/mdx"],
+    },
+    {
+      type: "category",
+      label: "System Design",
+      items: ["system-design/mdx"],
+    },
+    {
+      type: "category",
+      label: "Web Peformance",
+      items: ["web-performance/mdx"],
+    },
+  ],
 };
